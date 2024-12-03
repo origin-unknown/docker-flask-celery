bec23669d24902132cf5b3faff0f645ce3630f0c
origin-unknown <ich.hier.wer.dort@gmx.de>
2024-12-03 16:55:41 +0100

    Changing Database-URI as environment variable.

--- services/flask_app/src/app.py
+++ services/flask_app/src/app.py
@@ -39,7 +39,7 @@ app.config.from_mapping(
 		result_backend=os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost'), 
 		task_ignore_result=True, 
 	), 
-	SQLALCHEMY_DATABASE_URI='sqlite:///uploads.db', 
+	SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URI', 'sqlite:///uploads.db'), 
 	UPLOAD_FOLDER=os.path.join(app.instance_path, 'uploads'), 
 	MAX_CONTENT_LENGTH=16 * 1024 * 1024, 
 )
